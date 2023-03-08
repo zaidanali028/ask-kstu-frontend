0fe0b2a88411d21d7dfa63b42ecee62c1be63b8b
Zamani2025 <iddishani1@gmail.com>
2023-03-08 12:18:51 -0800

    make it better

--- lib/models/constant.dart
+++ lib/models/constant.dart
@@ -1,10 +1,10 @@
-const baseUrl = 'http://192.168.73.128:8000/api';
+const baseUrl = 'http://192.168.3.128:8000/api';
 
 const loginUrl = baseUrl + '/login';
 const registerUrl = baseUrl + '/register';
 const logoutUrl = baseUrl + '/logout';
 const userUrl = baseUrl + '/user';
 
-const serverError = "Server error";
+const serverError = "Check your internet connection";
 const unauthorized = "Unauthorized";
 const somethingWentwrong = "Something went wrong, try again";
